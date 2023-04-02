9176f06a12c289e7cfa56acd2f9b6e794a4851e8
Ash-Cassandra <c.larcheveque@orange.fr>
2023-04-02 21:10:25 +0200

    media query banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -6,8 +6,9 @@ function Banner() {
     return (
       <div className='banner'>        
         <img src={imgBanner} className='imgBanner bothPictures' alt="falaises" />
-        <img src={background} className='backgroundBanner bothPictures' />
+        <img src={background} className='backgroundBanner bothPictures' alt="#"/>
         <h1 className='titleBanner'>Chez vous, <br /> partout et ailleurs</h1>
+        <h1 className='titleBannerDesktop'>Chez vous, partout et ailleurs</h1>
       </div>)}
 
 export default Banner
