c5a0ea72b97a64dcfb32f92068ad26538d262b5d
Ash-Cassandra <c.larcheveque@orange.fr>
2023-05-04 16:24:25 +0200

    supression de fichier inutilisé

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-
 import App from './App';
 import Header from './components/Header/Header';
 import About from './pages/About/About';
 import Error404 from './components/Error404/Error404'
-import Lodging from './components/Lodging/Lodging';
 import Sheet from './components/Sheet/Sheet'
 import LodgingProvider from './components/Lodging/Lodging';
 
@@ -22,7 +20,6 @@ root.render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/About" element={<About />} />
-        <Route path="/Lodging" element={<Lodging />}/>
         <Route path="/Sheet/:id" element={<Sheet />}/>
         <Route path="*" element={<Error404 />} />
       </Routes>
